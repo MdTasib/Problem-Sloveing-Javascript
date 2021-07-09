73113ac201c15f34cc687334382628ed9f30b107
MdTasib <ohidulalam442@gmail.com>
2021-07-09 16:39:00 +0600

    comma operator

---
+++ js/lesson-two/six.js
@@ -0,0 +1,8 @@
+// compare two arrays by value
+
+const hasSameElements = (a, b) => {
+    return a.lenght === b.lenght && a.every((v, i) => v === b[i]);
+}
+
+console.log(hasSameElements([1, 2], [1, 2]));
+console.log(hasSameElements([1, 2], [2, 1]));
